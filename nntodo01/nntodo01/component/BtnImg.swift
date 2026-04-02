00c93e4e665cf2cc630e2d13d29fbbcbbe34e262
GWAN <116233013+KGWAN@users.noreply.github.com>
2026-04-02 15:25:01 +0900

    olii_todo 0402

--- nntodo01/nntodo01/component/BtnImg.swift
+++ nntodo01/nntodo01/component/BtnImg.swift
@@ -11,13 +11,16 @@ struct BtnImg: View {
     //init
     private let imgName: String
     private let color: Color
+    private let size: CGFloat
     private let action: () -> Void
     
     init(_ imgName: String = "",
          color: Color = .black,
+         size: CGFloat = 25,
          action: @escaping () -> Void) {
         self.imgName = imgName
         self.color = color
+        self.size = size
         self.action = action
     }
     
@@ -25,17 +28,19 @@ struct BtnImg: View {
         Button {
             action()
         } label: {
-            ImgSafe(imgName, color: color)
-                .frame(width: 22.5, height: 22.5)
-                .padding(2.5)
-                .background(.ultraThinMaterial)
-                .cornerRadius(15)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 15)
-                        .stroke(.white.opacity(0.2), lineWidth: 1)
-                }
-                .shadow(color: .black.opacity(0.1), radius: 2.5, x: 0, y: 0)
-                .padding(2.5)
+            HStack(spacing: 0) {
+                ImgSafe(imgName, color: color)
+                    .frame(width: size, height: size)
+            }
+            .frame(width: 25, height: 25)
+            .background(.ultraThinMaterial)
+            .cornerRadius(15)
+            .overlay {
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(.white.opacity(0.2), lineWidth: 1)
+            }
+            .shadow(color: .black.opacity(0.1), radius: 2.5, x: 0, y: 0)
+            .padding(2.5)
         }
     }
 }
