d606dedc0a1a7cbb923866829b174456a6f94ed3
GWAN <116233013+KGWAN@users.noreply.github.com>
2026-04-03 09:28:16 +0900

    color mark for kategory

--- nntodo01/nntodo01/dataModel/enum/MarkKategory.swift
+++ nntodo01/nntodo01/dataModel/enum/MarkKategory.swift
@@ -11,10 +11,19 @@ import UIKit
 
 enum ColorMarkKategory: String, CaseIterable, Identifiable {
     case red = "#ff0000"
+    case lightcoral = "#F08080"
     case orange = "#ffa500"
-//    case yellow = "#ffff00"
+    case moccasin = "#FFE4B5"
+    case yellow = "#ffff00"
+    case greenyellow = "#ADFF2F"
     case green = "008000"
+    case steelblue = "#4682B4"
     case blue = "#0000ff"
+    case mediumblue = "#0000CD"
+    case navy = "#000080"
+    case mediumorchid = "#BA55D3"
+    case purple = "#800080"
+    case pink = "#FFC0CB"
     
     var id: Self { self }
     
